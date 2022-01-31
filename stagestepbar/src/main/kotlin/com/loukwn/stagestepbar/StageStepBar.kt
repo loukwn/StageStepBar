36ae03017e1695b2761c1c7310cbae850e736d22
loukwn <loukwn@gmail.com>
2022-01-31 22:46:29 +0000

    feat: revert stagestepbar code change to make tests pass

--- stagestepbar/src/main/kotlin/com/loukwn/stagestepbar/StageStepBar.kt
+++ stagestepbar/src/main/kotlin/com/loukwn/stagestepbar/StageStepBar.kt
@@ -794,7 +794,7 @@ class StageStepBar @JvmOverloads constructor(
     private fun getDefaultConfig(): StageStepBarConfig {
         return StageStepBarConfig(
             stageStepConfig = listOf(50, 50),
-            currentState = null,
+            currentState = State(0, 21),
             shouldAnimate = true,
             animationDuration = 500L,
             orientation = Orientation.Horizontal,
